f6f2d49a3402f7f4b0cce32c120926b60e710ced
usertirrek06 <71853456+usertirrek06@users.noreply.github.com>
2022-07-25 19:17:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> sayilar = new MyList<int>();
+
+            sayilar.Add(3);
+            
+            sayilar.Remove(3);
+            foreach (var item in sayilar.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
