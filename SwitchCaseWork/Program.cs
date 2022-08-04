68701b1c048046ef09e819f6d24fc0c85bd2ef65
usertirrek06 <71853456+usertirrek06@users.noreply.github.com>
2022-08-05 00:17:06 +0300

    Switch-Case

---
+++ SwitchCaseWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwitchCaseWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            switch (a)
+            {
+                case 10:
+                    Console.WriteLine(a);
+                    break;
+                case 1:
+                    Console.WriteLine("Wrong");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
